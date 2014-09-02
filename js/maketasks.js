acedc01bdbd1537535ebca2781dbc0c72a4aa21c
Audrey Brockhaus <abrockhaus@abrockhaus.local>
2014-09-01 20:02:09 -0700

    added completed list

--- js/maketasks.js
+++ js/maketasks.js
@@ -1,25 +1,26 @@
 //construct tasks
 (function() {
 
-  function TaskItem(taskParent, taskName, priority, dueDate, status){
+  function TaskItem(taskName, priority, dueIndex, dueDate, status){
     this.data = {};
     this.data.name = taskName;
     this.data.priority = priority;
+    this.data.dueIndex = Number(dueIndex);
     this.data.dueDate = dueDate;
     this.data.status = status;
-    this.writeNewTask(taskParent, taskName, priority, dueDate, status);
+    this.writeNewTask(taskName, priority, dueDate, status);
     this.attachEvents();
     return this;
   }
 
-  TaskItem.prototype.writeNewTask = function(taskParent, taskName, priority, dueDate, status){
+  TaskItem.prototype.writeNewTask = function(taskName, priority, dueDate, status){
     var taskHeading = document.createElement("h3"),
     dueDisplay = document.createElement("p");
     this.deleteButton = document.createElement("button");
     this.completeButton = document.createElement("button");
+    this.pendingButton = document.createElement("button");
     this.el = document.createElement("li");
     this.el.classList.add(priority);
-    this.el.classList.add(status);
     taskHeading.innerHTML = taskName;
     this.el.appendChild(taskHeading);
     dueDisplay.innerHTML = dueDate;
@@ -28,10 +29,11 @@
     this.deleteButton.innerHTML = "delete";
     this.el.appendChild(this.deleteButton);
     this.completeButton.classList.add("completed");
-    this.completeButton.innerHTML = "Completed?";
+    this.completeButton.innerHTML = "Complete this task";
     this.el.appendChild(this.completeButton);
-    console.log(priority);
-    taskParent.el.appendChild(this.el);
+    this.pendingButton.classList.add("pendingButton");
+    this.pendingButton.innerHTML = "Start task again";
+    this.el.appendChild(this.pendingButton);
   };
 
   TaskItem.prototype.setOnDeleteCallback = function(callback) {
@@ -42,6 +44,10 @@
     this.onCompleteCallback = callback;
   };
 
+  TaskItem.prototype.setOnPendingCallback = function(callback) {
+    this.onPendingCallback = callback;
+  };
+
   TaskItem.prototype.attachEvents = function() {
     var self = this;
     self.deleteButton.addEventListener("click", function() {
@@ -50,8 +56,11 @@
     });
 
     self.completeButton.addEventListener("click", function() {
-      self.el.classList.add("complete");
       self.onCompleteCallback();
+    });
+
+    self.pendingButton.addEventListener("click", function() {
+      self.onPendingCallback();
     });
   };
 
--- js/tasklist.js
+++ js/tasklist.js
@@ -5,33 +5,62 @@
     var submit = document.getElementById("submit"),
     deleteAll = document.getElementById("deleteAll"),
     task = document.getElementById("task");
-    this.dueDates = document.getElementById("date"); //for some reason this returns undefined in other functions without "this" while task is just fine without it
+    this.dueDates = document.getElementById("date");
     this.db = window.localStorage;
     this.taskDataString = this.db.getItem("yourStoredTasks");
     this.taskList =[];
-    this.createList();
+    this.createLists();
     this.loadSavedTasks();
     this.attachEvents();
   }
 
-  TaskList.prototype.createList = function(){
-    this.el = document.createElement("ul");
-    this.el.setAttribute("id", "taskUl");
+  TaskList.prototype.createLists = function(){
+    var pendingHeading = document.createElement("h3"),
+    completedHeading = document.createElement("h3");
+
+    //pending tasks list
+    pendingHeading.innerHTML = "Pending Tasks";
+    
+    this.taskUl = document.createElement("ul");
+    this.taskUl.setAttribute("id", "taskUl");
+
+    this.el = document.createElement("div");
+    this.el.setAttribute("id", "pendingDiv");
+
+    this.el.appendChild(pendingHeading);
+    this.el.appendChild(this.taskUl);
+
     document.body.appendChild(this.el);
+
+    //completed tasks list
+    completedHeading.innerHTML = "Completed Tasks";
+    
+    this.completedUl = document.createElement("ul");
+    this.completedUl.setAttribute("id", "completedUl");
+
+    this.completedList = document.createElement("div");
+    this.completedList.setAttribute("id", "completedDiv");
+
+    this.completedList.appendChild(completedHeading);
+    this.completedList.appendChild(this.completedUl);
+
+    document.body.appendChild(this.completedList);
   };
 
   TaskList.prototype.loadSavedTasks = function() {
-    var taskParent = this;
-    if (taskParent.taskDataString) {
-      var taskData = JSON.parse(taskParent.taskDataString);
+    var self = this;
+    if (self.taskDataString) {
+      var taskData = JSON.parse(self.taskDataString);
       for (var i=0; i < taskData.length; i++) {
         var name = taskData[i].name;
+        var dueIndex = taskData[i].dueIndex;
         var dueDate = taskData[i].dueDate;
         var priority = taskData[i].priority;
         var status = taskData[i].status;
-        var newTaskItem = new TaskItem(taskParent, name, priority, dueDate, status);
-        taskParent.activate(newTaskItem);
+        var newTaskItem = new TaskItem(name, priority, dueIndex, dueDate, status);
+        this.activate(newTaskItem);
         this.setTaskObjectCallbacks(newTaskItem);
+        this.renderList();
       }
     }
   };
@@ -48,21 +77,22 @@
   };
 
   TaskList.prototype.requestNewTask= function(){
-    var taskParent = this,
-    taskName = task.value,
+    var taskName = task.value,
+    dueIndex = this.dueDates.selectedIndex,
     dueDate = this.dueDates.options[this.dueDates.selectedIndex].value,
     priority = document.querySelector('input[name = "priority"]:checked').value,
     status = "pending";
-    var newTaskItem = new TaskItem(taskParent, taskName, priority, dueDate, status);
-    taskParent.activate(newTaskItem);
+    var newTaskItem = new TaskItem(taskName, priority, dueIndex, dueDate, status);
+    this.activate(newTaskItem);
     this.save(newTaskItem);
     this.setTaskObjectCallbacks(newTaskItem);
+    this.renderList();
   };
 
   TaskList.prototype.attachEvents = function(){
     self = this; 
     submit.addEventListener("click", function(){self.validate(); task.focus(); }, false);
-    task.onkeydown = function(event){
+    task.addEventListener("keydown", function(event){
       if(event.which == 13 || event.keyCode == 13){
         self.validate();
         task.value = null;
@@ -72,8 +102,8 @@
       else{
         return true;
       }
-    };
-    deleteAll.addEventListener("click", function(){self.db.setItem("yourStoredTasks", []); self.el.innerHTML = "";}, false);
+    });
+    deleteAll.addEventListener("click", function(){self.db.setItem("yourStoredTasks", []); self.taskUl.innerHTML = ""; self.completedUl.innerHTML = "";});
   };
 
   TaskList.prototype.save = function() {
@@ -82,6 +112,29 @@
       dataArray.push(taskItem.data);
     });
     this.db.setItem("yourStoredTasks", JSON.stringify(dataArray));
+  };
+
+  TaskList.prototype.renderList = function() {
+    var self = this;
+    this.taskList.sort(this.sortByDate);
+    self.taskList.forEach(function(taskItem) {
+      if(taskItem.data.status == "pending"){
+        taskItem.el.classList.remove("complete");
+        taskItem.el.classList.add("pending");
+        self.taskUl.appendChild(taskItem.el);
+      }
+      else{
+        taskItem.el.classList.remove("pending");
+        taskItem.el.classList.add("complete");
+        self.completedUl.appendChild(taskItem.el);
+      }
+    });
+  };
+
+  TaskList.prototype.sortByDate = function(a, b) {
+    var aIndex = a.data.dueIndex;
+    var bIndex = b.data.dueIndex;
+    return ((aIndex < bIndex) ? -1 : ((aIndex > bIndex) ? 1 : 0));
   };
 
   TaskList.prototype.activate = function(newTaskObject) {
@@ -100,6 +153,14 @@
       var index = self.taskList.indexOf(newTaskObject);
       self.taskList[index].data.status = "complete";
       self.save();
+      self.renderList();
+    });
+
+    newTaskObject.setOnPendingCallback(function() {
+      var index = self.taskList.indexOf(newTaskObject);
+      self.taskList[index].data.status = "pending";
+      self.save();
+      self.renderList();
     });
   };
 
